21e703dcc161961a8671d20ea845ef05f0a17fcb
nappy <85632877+Nappy97@users.noreply.github.com>
2023-06-27 18:21:49 +0900

    feat:
    https://learn.microsoft.com/ko-kr/training/modules/persist-data-ef-core/4-interacting-data
    fin
    4/7

--- ContosoPizza/Program.cs
+++ ContosoPizza/Program.cs
@@ -1,4 +1,6 @@
+using ContosoPizza.Data;
 using ContosoPizza.Services;
+
 // Additional using declarations
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +10,9 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Add the PizzaContext
+// PizzaContext를 ASP.NET Core의 종속성 삽입 시스템에 등록합니다.
+// PizzaContext가 SQLite 데이터베이스 공급자를 사용하도록 지정합니다.로컬 파일 ContosoPizza.db를 가리키는 SQLite 연결 문자열을 정의합니다.
+builder.Services.AddSqlite<PizzaContext>("Data Source=ContosoPizza.db");
 
 // Add the PromotionsContext
 
@@ -27,6 +32,7 @@ app.UseAuthorization();
 app.MapControllers();
 
 // Add the CreateDbIfNotExists method call
+app.CreateDbIfNotExists();
 
 app.MapGet("/", () => @"Contoso Pizza management API. Navigate to /swagger to open the Swagger test UI.");
 
--- ContosoPizza/Services/PizzaService.cs
+++ ContosoPizza/Services/PizzaService.cs
@@ -1,41 +1,87 @@
+using ContosoPizza.Data;
 using ContosoPizza.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ContosoPizza.Services;
 
 public class PizzaService
 {
-    public PizzaService()
+    private readonly PizzaContext _context;
+
+    public PizzaService(PizzaContext context)
     {
-        
+        _context = context;
     }
 
     public IEnumerable<Pizza> GetAll()
     {
-        throw new NotImplementedException();
+        return _context.Pizzas
+            .AsNoTracking()
+            .ToList();
     }
 
     public Pizza? GetById(int id)
     {
-        throw new NotImplementedException();
+        return _context.Pizzas
+            .Include(p => p.Toppings)
+            .Include(p => p.Sauce)
+            .AsNoTracking()
+            .SingleOrDefault(p => p.Id == id);
     }
 
     public Pizza? Create(Pizza newPizza)
     {
-        throw new NotImplementedException();
+        _context.Pizzas.Add(newPizza);
+        _context.SaveChanges();
+
+        return newPizza;
     }
 
     public void AddTopping(int PizzaId, int ToppingId)
     {
-        throw new NotImplementedException();
+        var pizzaToUpdate = _context.Pizzas.Find(PizzaId);
+        var toppingToAdd = _context.Toppings.Find(ToppingId);
+
+        if (pizzaToUpdate is null || toppingToAdd is null)
+        {
+            throw new InvalidOperationException("피자나 소스가 존재하지 않습니다");
+        }
+
+        if (pizzaToUpdate.Toppings is null)
+        {
+            // TODO
+            // 비어 있다면 빈 리스트자체를 담아서 저장....?
+            pizzaToUpdate.Toppings = new List<Topping>();
+        }
+        
+        pizzaToUpdate.Toppings.Add(toppingToAdd);
+        _context.SaveChanges();
     }
 
+    // Add sauce의 역할 (표시만 update라고 한듯함 sauce를 바꾸는게 아닌 피자의 소스를 바꾸는것)
     public void UpdateSauce(int PizzaId, int SauceId)
     {
-        throw new NotImplementedException();
+        // Find 메소드는 기본키(PK) 조회
+        var pizzaToUpdate = _context.Pizzas.Find(PizzaId);
+        var sauceToUpdate = _context.Sauces.Find(SauceId);
+
+        if (pizzaToUpdate is null || sauceToUpdate is null)
+        {
+            throw new InvalidOperationException("피자나 소스가 존재하지 않습니다.");
+        }
+
+        pizzaToUpdate.Sauce = sauceToUpdate;
+        _context.SaveChanges();
     }
 
     public void DeleteById(int id)
     {
-        throw new NotImplementedException();
+        var pizzaToDelete = _context.Pizzas.Find(id);
+
+        if (pizzaToDelete is not null)
+        {
+            _context.Pizzas.Remove(pizzaToDelete);
+            _context.SaveChanges();
+        }
     }
 }
